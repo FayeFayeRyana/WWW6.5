870aa49bedb1c40bc986ce0d7b0eda8ef083ab6f
FayeFaye <ryana14520@gmail.com>
2026-03-14 11:25:12 +0800

    Update Day13-2-TokenSale.sol
    correct the import path

--- FayeFaye飞飞/Day13-2-TokenSale.sol
+++ FayeFaye飞飞/Day13-2-TokenSale.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./SimpleERC20.sol";
+import "./Day13-1-SimpleERC20.sol";
 
 contract SimplifiedTokenSale is SimpleERC20 {
     uint256 public tokenPrice;
